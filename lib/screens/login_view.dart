629107c6727320028f1e43cd169467b2c82e03c8
Dinuka_prasad <127348943+DinukaPrasad@users.noreply.github.com>
2025-04-30 03:39:14 +0530

    Update login_view.dart

--- lib/screens/login_view.dart
+++ lib/screens/login_view.dart
@@ -1,5 +1,12 @@
+import 'dart:math';
+
 import 'package:eltracker_app/home_page.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:eltracker_app/controller/auth_service.dart';
+
+import 'package:eltracker_app/screens/register_view.dart';
+
 
 class LoginView extends StatefulWidget {
   const LoginView({super.key});
@@ -10,11 +17,27 @@ class LoginView extends StatefulWidget {
 
 class _LoginViewState extends State<LoginView> {
 
-  var userName = TextEditingController();
+  var email = TextEditingController();
   var password = TextEditingController();
 
-  var inputUserName = '';
-  var inputPassword = '';
+  void login() async {
+
+    try {
+        await authService.value.singIn(email: email.text.trim(), password: password.text.trim());
+
+        if (mounted) {
+          Navigator.pushReplacement(
+            context, 
+            MaterialPageRoute(builder: (context)=>HomePage(username: email.text)),
+          );
+        }
+
+    } on FirebaseException catch (e) {
+          print(e);
+    }
+
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -31,7 +54,7 @@ class _LoginViewState extends State<LoginView> {
             //*user name area
             Text('User Name'),
             TextField(
-              controller: userName,
+              controller: email,
               decoration: InputDecoration(
                 labelText: 'Enter Username',
                 border: OutlineInputBorder(),
@@ -55,18 +78,16 @@ class _LoginViewState extends State<LoginView> {
             ElevatedButton(
               child: Text('Login'),
               onPressed: () {
-
-                inputUserName = userName.text;
-                inputPassword = password.text;
-
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => HomePage(username: inputUserName,))
-                );
-
-                // ignore: avoid_print
-                print('clicked login button');
+                login();
               }, 
+              ),
+
+              TextButton(
+                onPressed: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>RegisterView()));
+                },
+
+                child: Text('Don\'t have an account'),
               ),
           ],
         ),
