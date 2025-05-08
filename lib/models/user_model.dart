55cc49540d519c70cc9c29e44dc248bf22c2e55a
Dinuka_prasad <127348943+DinukaPrasad@users.noreply.github.com>
2025-05-08 13:05:28 +0530

    Update user_model.dart

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -1,8 +1,42 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class UserModel {
-
   final String name;
-  final String mail;
+  final int age;
+  final DateTime createdAt;
+  final DateTime updatedAt;
 
-  UserModel({ required this.name , required this.mail});
+  UserModel({
+    required this.name,
+    required this.age,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+  }) : createdAt = createdAt ?? DateTime.now(),
+       updatedAt = updatedAt ?? DateTime.now();
 
+  UserModel.fromJson(Map<String, Object?> json)
+    : this(
+        name: json['name']! as String,
+        age: json['age']! as int,
+        createdAt: (json['createdAt'] as Timestamp).toDate(),
+        updatedAt: (json['updatedAt'] as Timestamp).toDate(),
+      );
+
+  UserModel copyWith({String? name, int? age, DateTime? updatedAt}) {
+    return UserModel(
+      name: name ?? this.name,
+      age: age ?? this.age,
+      createdAt: createdAt,
+      updatedAt: updatedAt ?? DateTime.now(),
+    );
+  }
+
+  Map<String, Object?> toJson() {
+    return {
+      'name': name,
+      'age': age,
+      'createdAt': Timestamp.fromDate(createdAt),
+      'updatedAt': Timestamp.fromDate(updatedAt),
+    };
+  }
 }
