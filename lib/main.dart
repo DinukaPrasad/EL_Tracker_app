1d9c0b96711187b9ffbb4378395798b65f749c1d
Dinuka_prasad <127348943+DinukaPrasad@users.noreply.github.com>
2025-05-08 13:04:34 +0530

    Update with fireStore settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,20 @@
 import 'package:eltracker_app/config/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-//import 'package:eltracker_app/home_page.dart';
 import 'package:eltracker_app/screens/login_view.dart';
 
 Future<void> main() async {
-    WidgetsFlutterBinding.ensureInitialized();
-      await Firebase.initializeApp(
-      options: DefaultFirebaseOptions.currentPlatform,
-    );
-    runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  FirebaseFirestore.instance.settings = const Settings(
+    persistenceEnabled: true,
+  );
+  runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
@@ -28,4 +31,3 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
